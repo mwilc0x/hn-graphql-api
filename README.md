89f9da434027b33c57d15b5296f616dadf29f064
Mike Wilcox <mwilcox56@gmail.com>
2015-10-11 14:01:57 -0400

    Initial connection example working.

---
+++ README.md
@@ -0,0 +1,34 @@
+Exploring GraphQL
+=================
+
+## Install
+
+Install all dependencies with:
+
+```sh
+npm install
+```
+
+## Serve
+
+Begin the server with:
+
+```sh
+npm start
+```
+
+## Explore
+
+Open http://localhost:3100/ to explore requests with GraphiQL
+
+## Lint
+
+Validate the code with:
+
+```sh
+npm run lint
+```
+
+## License
+
+MIT
---
+++ server/index.js
@@ -0,0 +1,78 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+
+const app = express();
+
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} from 'graphql';
+
+import {
+  globalIdField,
+  connectionFromArray,
+  connectionArgs,
+  connectionDefinitions
+} from 'graphql-relay';
+
+import {
+  getItem,
+  getTopItems
+} from './data';
+
+const itemType = new GraphQLObjectType({
+  name: 'Item',
+  description: 'An item',
+  fields: () => ({
+    id: globalIdField('Item'),
+    name: {
+      type: GraphQLString,
+      description: 'The name of the item.',
+    }
+  })
+});
+
+function resolveItemConnection(topItem, args) {
+  return topItem.items.map((id) => getItem(id));
+}
+
+const {connectionType: itemConnection} =
+  connectionDefinitions({name: 'Item', nodeType: itemType});
+
+const topItemType = new GraphQLObjectType({
+  name: 'TopItem',
+  description: 'Top items list.',
+  fields: () => ({
+    id: globalIdField('TopItem'),
+    items: {
+      type: itemConnection,
+      description: 'The top items.',
+      args: connectionArgs,
+      resolve: (topItem, args) => connectionFromArray(
+        resolveItemConnection(topItem, args),
+        args
+      )
+    }
+  })
+});
+
+const queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    topItems: {
+      type: topItemType,
+      resolve: () => getTopItems(),
+    }
+  })
+});
+
+app.use('/graphql', graphqlHTTP({
+  schema: new GraphQLSchema({query: queryType}),
+  graphiql: true
+}));
+
+const server = app.listen(3100, () => {
+  console.log('GraphQL listening at http://localhost:3100');
+});
